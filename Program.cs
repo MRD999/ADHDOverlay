f5f450f539cfc7364aeb0deb6cf805b2effab09b
Jordan <Jordan@email>
2023-10-14 23:37:53 +1100

    +open notepad and set size +notepad moves accross the screen *need additional work on the x,y boundaries *want to move notpad to its own class * add additional programs

--- Program.cs
+++ Program.cs
@@ -1,11 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Runtime.InteropServices;
 using System.Diagnostics;
 using System.Threading;
+using System.Windows.Forms;
 
 namespace ADHDOverlay
 {
@@ -21,17 +18,56 @@ namespace ADHDOverlay
 
         static void Main(string[] args)
         {
-            //Process.Start("notepad", "readme.txt");
-            Thread.Sleep(6000);
+            //Get Screen size
+            int screenWidth = Screen.PrimaryScreen.Bounds.Width;
+            int screenHeight = Screen.PrimaryScreen.Bounds.Height;
+            Console.WriteLine("Screen Width: " + screenWidth);
+            Console.WriteLine("Screen Height: " + screenHeight);
+            //open notepad
+            Process notePadProcess = Process.Start("notepad", "readme.txt");
+            Thread.Sleep(1000);
             // Find (the first-in-Z-order) Notepad window.
             IntPtr hWnd = FindWindow("Notepad", null);
 
             // If found, position it.
             if (hWnd != IntPtr.Zero)
             {
-                // Move the window to (0,0) without changing its size or position
-                // in the Z order.
-                SetWindowPos(hWnd, IntPtr.Zero, 0, 0, 0, 0, 0x0001 | 0x0004);
+                int windowX = 0;
+                int windowY = 0;
+                int changeX = 1;
+                int changeY = 1;
+                int appWidth = 150;
+                int appHeight = 150;
+                SetWindowPos(hWnd, IntPtr.Zero, 0, 0, appWidth, appHeight, 0x0002 | 0x0004);
+                Thread.Sleep(1000);
+                while (true)
+                {
+                    SetWindowPos(hWnd, IntPtr.Zero, windowX, windowY, 0, 0, 0x0001 | 0x0004);
+                    if(windowX+appWidth >= screenWidth)
+                    {
+                        changeX = -1;
+                    }
+                    if (windowX <= 0)
+                    {
+                        changeX = 1;
+                    }
+                    if (windowY + appHeight >= screenHeight)
+                    {
+                        changeY = -1;
+                    }
+                    if (windowY <= 0)
+                    {
+                        changeY = 1;
+                    }
+                    windowX += changeX;
+                    windowY += changeY;
+                    Console.WriteLine(windowX +" "+ windowY);
+                    Thread.Sleep(25);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Error window not found");
             }
 
         }
